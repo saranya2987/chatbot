b39fda404927e1f78f6b96a9b226b162fc14f673
saranya2987 <114751257+saranya2987@users.noreply.github.com>
2025-05-05 10:16:59 +0530

    Add files via upload

---
+++ build/precache-manifest.93dae812c9ffb4305b7c34e86a05948f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c2c95e0f52a70e7087fd721c090071b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4084fcca6aa1d7cf68cf",
+    "url": "/static/css/main.cbb491f8.chunk.css"
+  },
+  {
+    "revision": "d366a841377b22d38c87",
+    "url": "/static/js/2.be86ae59.chunk.js"
+  },
+  {
+    "revision": "648e5a1a81a83d12984065ecc097faab",
+    "url": "/static/js/2.be86ae59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4084fcca6aa1d7cf68cf",
+    "url": "/static/js/main.7269a531.chunk.js"
+  },
+  {
+    "revision": "f1184cc136655f683e89",
+    "url": "/static/js/runtime-main.26413b77.js"
+  }
+]);
